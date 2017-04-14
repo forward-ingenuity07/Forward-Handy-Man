c8dda482869c62fc0cf5af4f2d4127cd34efd3b1
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-14 18:08:12 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/index.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/index.js
@@ -33,14 +33,16 @@
 
         //goog.require('goog.dom');
         var today = new Date();
+
         var dd = today.getDate();
         var mm = today.getMonth() + 1;//January is 0, so always add + 1
         var yyyy = today.getFullYear();
+        var hou = today.getTime();
         if (dd < 10) { dd = '0' + dd }
         if (mm < 10) { mm = '0' + mm }
         today = mm + '/' + dd + '/' + yyyy;
         document.getElementById('Checking').onclick= function() {
-            alert("Today's date is: "+ today)
+            alert("Today's date is: " + today + "\n" +"The time is: "+ hou);
 
         }
     };
