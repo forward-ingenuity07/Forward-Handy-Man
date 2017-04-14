bd926e2c0531daf4895ddd33f52eec3a265f9be2
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-14 20:20:33 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/index.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/index.js
@@ -21,7 +21,22 @@
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
+        var today = new Date();
+        var hous = today.getHours();
+        var mins = today.getMinutes();
+        if (hous < 12)
+        {
+            alert('Good morning Mr Kafwilo');
+        }
+        else if (hous >= 12 && hous < 18)
+        {
+            alert('Good afternoon Mr Kafwilo');
+        }
+        else if (hous >= 18)
+        {
+            alert('Good evening Mr Kafwilo');
+        }
+             
         /*document.getElementById('Checking').onclick = function () {
             function sayHi() {
                 var newHeader = goog.dom.createDom('p', { 'style': 'color:white' },
@@ -40,7 +55,10 @@
             var mm = today.getMonth() + 1;//January is 0, so always add + 1
             var yyyy = today.getFullYear();
             var hou = today.getHours();
+            if (hou < 10) { hou = '0' + hou; }
+            
             var min = today.getMinutes();
+            if (min < 10) { min = '0' + min;}
             if (dd < 10) { dd = '0' + dd }
             if (mm < 10) { mm = '0' + mm }
             today = mm + '/' + dd + '/' + yyyy;
