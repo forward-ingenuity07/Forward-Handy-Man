ced71345ebf9a0012371b89084d1492ed3a280cc
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-14 18:35:50 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/index.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/index.js
@@ -32,17 +32,20 @@
         };*/
 
         //goog.require('goog.dom');
-        var today = new Date();
 
-        var dd = today.getDate();
-        var mm = today.getMonth() + 1;//January is 0, so always add + 1
-        var yyyy = today.getFullYear();
-        var hou = today.getHours();
-        var min = today.getMinutes();
-        if (dd < 10) { dd = '0' + dd }
-        if (mm < 10) { mm = '0' + mm }
-        today = mm + '/' + dd + '/' + yyyy;
-        document.getElementById('Checking').onclick= function() {
+        document.getElementById('Checking').onclick = function () {
+            var today = new Date();
+
+            var dd = today.getDate();
+            var mm = today.getMonth() + 1;//January is 0, so always add + 1
+            var yyyy = today.getFullYear();
+            var hou = today.getHours();
+            var min = today.getMinutes();
+            if (dd < 10) { dd = '0' + dd }
+            if (mm < 10) { mm = '0' + mm }
+            today = mm + '/' + dd + '/' + yyyy;
+
+
             alert("Today's date is: " + today + "\n" +"The time is: "+ hou+":"+min);
 
         }
