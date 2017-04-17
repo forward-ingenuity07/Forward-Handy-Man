d4279106d73923452574537311df8a60556a4860
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-17 12:17:05 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/index.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/index.js
@@ -10,7 +10,7 @@
 
     function onDeviceReady() {
         //    alert('G')
-        alert('Working');
+        swal('Working');
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener('resume', onResume.bind(this), false);
@@ -36,12 +36,23 @@
         {
             swal('Good evening Mr Kafwilo');
         }
-        document.getElementById('check').onclick = function () {
-            scr = window.screen.height;
-            swal(scr);
+        document.getElementById('ButSchedule').onclick = function () {
+
+            alert('Displaying schedule');
 
         }
-             
+        document.getElementById('ButEvents').onclick = function () {
+
+            alert('Displaying Upcoming Events');
+
+        }
+        
+
+        document.getElementById('ButMaps').onclick = function () {
+
+            alert('Displaying Maps');
+
+        }
         /*document.getElementById('Checking').onclick = function () {
             function sayHi() {
                 var newHeader = goog.dom.createDom('p', { 'style': 'color:white' },
@@ -53,25 +64,7 @@
 
         //goog.require('goog.dom');
 
-        document.getElementById('Checking').onclick = function () {
-            var today = new Date();
-
-            var dd = today.getDate();
-            var mm = today.getMonth() + 1;//January is 0, so always add + 1
-            var yyyy = today.getFullYear();
-            var hou = today.getHours();
-            if (hou < 10) { hou = '0' + hou; }
-            
-            var min = today.getMinutes();
-            if (min < 10) { min = '0' + min;}
-            if (dd < 10) { dd = '0' + dd }
-            if (mm < 10) { mm = '0' + mm }
-            today = mm + '/' + dd + '/' + yyyy;
-
-
-            swal("Today's date is: " + today + "\n" +"The time is: "+ hou+":"+min);
-
-        }
+        
     };
 
     function onPause() {
