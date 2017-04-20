62d973777fab0714b39c8532ed854f3e0795dd72
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-20 13:37:07 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
@@ -1,15 +1,16 @@
-﻿
-
-
+﻿(function () {
+var Naam;
 document.getElementById('ButSub').onclick=function()
 {
     var Naam = document.getElementById('Naming');
-    if (typeof (Storage) !== "undefined") {
-        // Store
-        localStorage.setItem("lastname", Naam);
-        // Retrieve
-        document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-    } else {
-        document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-    }
+    
 }
+if (typeof (Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("lastname", Naam);
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+} else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
+})();
