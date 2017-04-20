3897c45f703751c45070c08ce1523aa7c493d985
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-20 13:30:49 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
@@ -1,16 +1,15 @@
 ﻿
 
-var Naam;
+
 document.getElementById('ButSub').onclick=function()
 {
-    Naam = document.getElementById('Naming');
-
-}
-if (typeof (Storage) !== "undefined") {
-    // Store
-    localStorage.setItem("lastname", Naam);
-    // Retrieve
-    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-} else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+    var Naam = document.getElementById('Naming');
+    if (typeof (Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("lastname", Naam);
+        // Retrieve
+        document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
 }
