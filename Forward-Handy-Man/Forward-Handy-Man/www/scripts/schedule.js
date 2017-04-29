ab8888f1c2a1995c91f6fd38d5059948a927f69c
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-29 23:09:12 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
@@ -12,7 +12,7 @@ document.getElementById('ButSub').onclick=function()
     //Naam = document.getElementById('Naming');
     //window.localStorage.setItem('Naming', Naam.value);
     Arr[0] = document.getElementById('Naming');
-    window.localStorage.setItem('Naming', (JSON.stringify(Arr)).value);
+    window.localStorage.setItem('Naming', JSON.stringify(Arr));
     alert('Submitted');
     //var Naam = document.getElementById('Naming');
     //window.localStorage.setItem('Naming', Naam.value);
@@ -39,7 +39,7 @@ document.getElementById('ButSub').onclick=function()
     }
     var StoredArr = JSON.parse(window.localStorage.getItem('Naming'));
     
-    document.getElementById("first_elem").innerHTML = StoredArr[0];
+    document.getElementById("first_elem").innerHTML = StoredArr[0].value;
     /*document.getElementById("sec_elem").innerHTML = StoredArr[1];
     document.getElementById("thi_elem").innerHTML = StoredArr[2];
     document.getElementById("fou_elem").innerHTML = StoredArr[3];
