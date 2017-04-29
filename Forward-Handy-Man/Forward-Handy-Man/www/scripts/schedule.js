0c5761fcc3b71e51c14e37ed01e31165d8b79c28
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-29 17:18:50 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
@@ -1,4 +1,58 @@
-﻿(function () {
+﻿
+document.getElementById('ButPlus').onclick = function () {
+    var timer, fullText, currentOffset, onComplete, wordSet;
+
+    function Speak(person, text, callback) {
+        //$("#name").html(person);
+        fullText = text;
+
+        wordSet = text.split('');
+        currentOffset = 0;
+        onComplete = callback;
+        timer = setInterval(onTick, 107);
+    }
+
+    function onTick() {
+        currentOffset++;
+        if (currentOffset == wordSet.length) {
+            complete();
+            return;
+        }
+        var text = "";
+
+        for (var i = 0; i < currentOffset; i++) {
+
+            text += wordSet[i];
+
+        }
+        text.trim();
+
+        $("#message").html(text);
+
+    }
+
+    function complete() {
+        clearInterval(timer);
+        timer = null;
+
+        $("#message").html(fullText);
+        if (onComplete) onComplete();
+    }
+
+    /*$(".box").click(function () {
+        complete();
+    });*/
+
+    Speak("Simon",
+        "         Enter an event below sir"
+    /*
+    function () {
+        setTimeout(function () {
+            Speak("Javascript", "Enter an event below");
+        }, 2000);
+    }*/);
+}
+(function () {
     var chosen_day;
     var chosen_time;
     var chosen_title;
@@ -75,56 +129,3 @@ document.getElementById('ButSub').onclick=function()
     document.getElementById("sev_elem").innerHTML = StoredArr[6];
     */
 })();
-document.getElementById('ButPlus').onclick=function(){
-var timer, fullText, currentOffset, onComplete, wordSet;
-
-function Speak(person, text, callback) {
-    //$("#name").html(person);
-    fullText = text;
-    
-    wordSet = text.split('');
-    currentOffset = 0;
-    onComplete = callback;
-    timer = setInterval(onTick, 107);
-}
-
-function onTick() {
-    currentOffset++;
-    if (currentOffset == wordSet.length) {
-        complete();
-        return;
-    }
-    var text = "";
-
-    for (var i = 0; i < currentOffset; i++) {
-        
-            text += wordSet[i];
-            
-    }
-    text.trim();
-    
-    $("#message").html(text);
-    
-}
-
-function complete() {
-    clearInterval(timer);
-    timer = null;
-    
-    $("#message").html(fullText);
-    if (onComplete) onComplete();
-}
-
-/*$(".box").click(function () {
-    complete();
-});*/
-
-Speak("Simon",
-    "         Enter an event below sir"
-/*
-function () {
-    setTimeout(function () {
-        Speak("Javascript", "Enter an event below");
-    }, 2000);
-}*/);
-}
