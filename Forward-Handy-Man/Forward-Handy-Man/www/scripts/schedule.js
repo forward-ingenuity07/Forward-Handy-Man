c7b932059d8cce83d76ec4cb1daa7697dca59923
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-29 22:22:28 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
@@ -1,4 +1,54 @@
 ﻿
+(function () {
+    var chosen_day;
+    var chosen_time;
+    var chosen_title;
+    
+
+document.getElementById('ButSub').onclick=function()
+{
+    var Naam;
+    Naam = document.getElementById('Naming');
+    window.localStorage.setItem('Naming', Naam.value);
+    
+    alert('Submitted');
+    //var Naam = document.getElementById('Naming');
+    //window.localStorage.setItem('Naming', Naam.value);
+    /*if (typeof (Storage) !== "undefined") {
+        // Store
+        
+        // Retrieve
+        
+        
+    }*/
+   /* else
+    {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }*/
+    
+}
+
+    document.getElementById('back').onclick=function()
+{
+        window.close();
+        
+        location.href = 'index.html';
+       
+    }
+    /*StoredArr = JSON.parse(window.localStorage.getItem('Naming'));
+    
+    document.getElementById("first_elem").innerHTML = StoredArr[0];
+    document.getElementById("sec_elem").innerHTML = StoredArr[1];
+    document.getElementById("thi_elem").innerHTML = StoredArr[2];
+    document.getElementById("fou_elem").innerHTML = StoredArr[3];
+    document.getElementById("fif_elem").innerHTML = StoredArr[4];
+    document.getElementById("six_elem").innerHTML = StoredArr[5];
+    document.getElementById("sev_elem").innerHTML = StoredArr[6];
+    */
+    document.getElementById("first_elem").innerHTML = window.localStorage.getItem('Naming');
+})();
+
+
 document.getElementById('ButPlus').onclick = function () {
     var timer, fullText, currentOffset, onComplete, wordSet;
 
@@ -52,80 +102,3 @@ document.getElementById('ButPlus').onclick = function () {
         }, 2000);
     }*/);
 }
-(function () {
-    var chosen_day;
-    var chosen_time;
-    var chosen_title;
-    
-
-document.getElementById('ButSub').onclick=function()
-{
-    var Naam;
-    if (day_chosen == 'Sunday')
-    {
-        Naam[0] = document.getElementById('Naming');
-
-    }
-
-    else if (day_chosen == 'Monday') {
-        Naam[1] = document.getElementById('Naming');
-
-    }
-
-    else if (day_chosen == 'Tuesday') {
-        Naam[2] = document.getElementById('Naming');
-
-    }
-    else if (day_chosen == 'Wednesday') {
-        Naam[3] = document.getElementById('Naming');
-
-    }
-    else if (day_chosen == 'Thursday') {
-        Naam[4] = document.getElementById('Naming');
-
-    }
-    else if (day_chosen == 'Friday') {
-        Naam[5] = document.getElementById('Naming');
-
-    }
-    else if (day_chosen == 'Saturday') {
-        Naam[6] = document.getElementById('Naming');
-
-    }
-    window.localStorage.setItem('Naming', JSON.stringify(Naam));
-    
-    alert('Submitted');
-    //var Naam = document.getElementById('Naming');
-    //window.localStorage.setItem('Naming', Naam.value);
-    /*if (typeof (Storage) !== "undefined") {
-        // Store
-        
-        // Retrieve
-        
-        
-    }*/
-   /* else
-    {
-        document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-    }*/
-    
-}
-
-    document.getElementById('back').onclick=function()
-{
-        window.close();
-        
-        location.href = 'index.html';
-       
-    }
-    StoredArr = JSON.parse(window.localStorage.getItem('Naming'));
-    StoredArr[1] = "EMK";
-    document.getElementById("first_elem").innerHTML = "EMK310";
-    document.getElementById("sec_elem").innerHTML = StoredArr[1];
-    document.getElementById("thi_elem").innerHTML = StoredArr[2];
-    document.getElementById("fou_elem").innerHTML = StoredArr[3];
-    document.getElementById("fif_elem").innerHTML = StoredArr[4];
-    document.getElementById("six_elem").innerHTML = StoredArr[5];
-    document.getElementById("sev_elem").innerHTML = StoredArr[6];
-    
-})();
