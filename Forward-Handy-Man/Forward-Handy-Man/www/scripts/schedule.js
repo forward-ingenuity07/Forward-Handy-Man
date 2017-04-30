f5f15d360e6cc8d95e0c37de4de1b5057d3b1b88
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-04-30 10:29:54 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
@@ -9,12 +9,13 @@ document.getElementById('ButSub').onclick=function()
 {
     var Naam;
     var Arr=[];
-    Naam = document.getElementById('Naming');
-    window.localStorage.setItem('Naming', Naam.value);
-    //Arr[0] = document.getElementById('Naming');
-    //window.localStorage.setItem('Naming', (JSON.stringify(Arr)));
+    //Naam = document.getElementById('Naming');
+   // window.localStorage.setItem('Naming', Naam.value);
+    Arr[0] = document.getElementById('Naming');
+    // window.localStorage.setItem('Naming', (JSON.stringify(Arr)));
+    window.localStorage["Naming"]=JSON.stringify(Arr);
     alert('Submitted');
-    var Naam = document.getElementById('Naming');
+   // var Naam = document.getElementById('Naming');
     //window.localStorage.setItem('Naming', Naam.value);
     /*if (typeof (Storage) !== "undefined") {
         // Store
@@ -37,8 +38,9 @@ document.getElementById('ButSub').onclick=function()
         location.href = 'index.html';
        
     }
-  //  var StoredArr = JSON.parse(window.localStorage.getItem('Naming'));
-   // document.getElementById("first_elem").innerHTML = StoredArr[0].value;
+    //var StoredArr = JSON.parse(window.localStorage.getItem('Naming'));
+    var StoredArr = JSON.parse(window.localStorage["Naming"]);
+    document.getElementById("first_elem").innerHTML = StoredArr[0].value;
     /*document.getElementById("sec_elem").innerHTML = StoredArr[1];
     document.getElementById("thi_elem").innerHTML = StoredArr[2];
     document.getElementById("fou_elem").innerHTML = StoredArr[3];
