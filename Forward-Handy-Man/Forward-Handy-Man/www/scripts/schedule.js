c9963c0aca5efee49447216195fbcc06183e3451
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-05-04 21:45:50 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
@@ -10,24 +10,7 @@
     document.getElementById('ButSub').onclick=function()
     {
 
-        if (window.localStorage.getItem('stand') != '3')
-        {
-            
-                for (j = 0; j < 10; j++)
-                    {
-                    window.localStorage.setItem('Local_day'+"0"+j,null);
-                    window.localStorage.setItem('Local_day' + "1" + j,null);
-                    window.localStorage.setItem('Local_day' + "2" + j,null);
-                    window.localStorage.setItem('Local_day' + "3" + j,null);
-                    window.localStorage.setItem('Local_day' + "4" + j,null);
-                    window.localStorage.setItem('Local_day' + "5" + j,null);
-                    window.localStorage.setItem('Local_day' + "6" + j,null);
-                
-                }
-
-
-            window.localStorage.setItem('stand', '3');
-        }
+      
         //var days = [["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""]];
         
       /*  for (i = 0; i < 10; i++)
@@ -45,42 +28,42 @@
 
         
         for (j = 0; j < 10; j++) {
-            if (window.localStorage.getItem('Local_day' + "0" + j) === null)
+            if (window.localStorage.getItem('Local_day' + "0" + j) == null)
             {
 
                 window.localStorage.setItem('Local_day' + "0" + j, " ");
                 
             }
             
-            if (window.localStorage.getItem('Local_day' + "1" + j) === null) {
+            if (window.localStorage.getItem('Local_day' + "1" + j) == null) {
 
                 window.localStorage.setItem('Local_day' + "1" + j, " ");
 
             }
 
-            if (window.localStorage.getItem('Local_day' + "2" + j) === null) {
+            if (window.localStorage.getItem('Local_day' + "2" + j) == null) {
 
                 window.localStorage.setItem('Local_day' + "2" + j, " ");
 
             }
 
-            if (window.localStorage.getItem('Local_day' + "3" + j) === null) {
+            if (window.localStorage.getItem('Local_day' + "3" + j) == null) {
 
                 window.localStorage.setItem('Local_day' + "3" + j, " ");
 
             }
-            if (window.localStorage.getItem('Local_day' + "4" + j) === null) {
+            if (window.localStorage.getItem('Local_day' + "4" + j) == null) {
 
                 window.localStorage.setItem('Local_day' + "4" + j, " ");
 
             }
 
-            if (window.localStorage.getItem('Local_day' + "5" + j) === null) {
+            if (window.localStorage.getItem('Local_day' + "5" + j) == null) {
 
                 window.localStorage.setItem('Local_day' + "5" + j, " ");
 
             }
-            if (window.localStorage.getItem('Local_day' + "6" + j) === null) {
+            if (window.localStorage.getItem('Local_day' + "6" + j) == null) {
 
                 window.localStorage.setItem('Local_day' + "6" + j, " ");
 
@@ -88,69 +71,69 @@
 
 
 
-            if (window.localStorage.getItem('Local_hour' + "0" + j) === null) {
+            if (window.localStorage.getItem('Local_hour' + "0" + j) == null) {
 
                 window.localStorage.setItem('Local_hour' + "0" + j, " ");
                 
             }
-            if (window.localStorage.getItem('Local_hour' + "1" + j) === null) {
+            if (window.localStorage.getItem('Local_hour' + "1" + j) == null) {
 
                 window.localStorage.setItem('Local_hour' + "1" + j, " ");
 
             }
-            if (window.localStorage.getItem('Local_hour' + "2" + j) === null) {
+            if (window.localStorage.getItem('Local_hour' + "2" + j) == null) {
 
                 window.localStorage.setItem('Local_hour' + "2" + j, " ");
 
             }
-            if (window.localStorage.getItem('Local_hour' + "3" + j) === null) {
+            if (window.localStorage.getItem('Local_hour' + "3" + j) == null) {
 
                 window.localStorage.setItem('Local_hour' + "3" + j, " ");
 
             }
-            if (window.localStorage.getItem('Local_hour' + "4" + j) === null) {
+            if (window.localStorage.getItem('Local_hour' + "4" + j) == null) {
 
                 window.localStorage.setItem('Local_hour' + "4" + j, " ");
 
             }
-            if (window.localStorage.getItem('Local_hour' + "5" + j) === null) {
+            if (window.localStorage.getItem('Local_hour' + "5" + j) == null) {
 
                 window.localStorage.setItem('Local_hour' + "5" + j, " ");
 
             }
-            if (window.localStorage.getItem('Local_hour' + "6" + j) === null) {
+            if (window.localStorage.getItem('Local_hour' + "6" + j) == null) {
 
                 window.localStorage.setItem('Local_hour' + "6" + j, " ");
 
             }
 
 
-            if (window.localStorage.getItem('Local_minute' + "0" + j) === null) {
+            if (window.localStorage.getItem('Local_minute' + "0" + j) == null) {
 
                 window.localStorage.setItem('Local_minute' + "0" + j, " ");
             }
 
-            if (window.localStorage.getItem('Local_minute' + "1" + j) === null) {
+            if (window.localStorage.getItem('Local_minute' + "1" + j) == null) {
 
                 window.localStorage.setItem('Local_minute' + "1" + j, " ");
             }
-            if (window.localStorage.getItem('Local_minute' + "2" + j) === null) {
+            if (window.localStorage.getItem('Local_minute' + "2" + j) == null) {
 
                 window.localStorage.setItem('Local_minute' + "2" + j, " ");
             }
-            if (window.localStorage.getItem('Local_minute' + "3" + j) === null) {
+            if (window.localStorage.getItem('Local_minute' + "3" + j) == null) {
 
                 window.localStorage.setItem('Local_minute' + "3" + j, " ");
             }
-            if (window.localStorage.getItem('Local_minute' + "4" + j) === null) {
+            if (window.localStorage.getItem('Local_minute' + "4" + j) == null) {
 
                 window.localStorage.setItem('Local_minute' + "4" + j, " ");
             }
-            if (window.localStorage.getItem('Local_minute' + "5" + j) === null) {
+            if (window.localStorage.getItem('Local_minute' + "5" + j) == null) {
 
                 window.localStorage.setItem('Local_minute' + "5" + j, " ");
             }
-            if (window.localStorage.getItem('Local_minute' + "6" + j) === null) {
+            if (window.localStorage.getItem('Local_minute' + "6" + j) == null) {
 
                 window.localStorage.setItem('Local_minute' + "6" + j, " ");
             }
@@ -168,7 +151,7 @@
             //Using Local_day
             for (i = 0; i < 10; i++)
             {
-                if(window.localStorage.getItem("Local_day" + "0" + i)===" ")
+                if(window.localStorage.getItem("Local_day" + "0" + i)==" ")
                 {
                     index = i;
                     break;
@@ -187,7 +170,7 @@
         else if (e.options[e.selectedIndex].value == "Monday") {
             //Using Local_day
             for (i = 0; i < 10; i++) {
-                if (window.localStorage.getItem("Local_day" + "1" + i)===" ") {
+                if (window.localStorage.getItem("Local_day" + "1" + i)==" ") {
                     index = i;
                     break;
 
@@ -206,7 +189,7 @@
         else if (e.options[e.selectedIndex].value == "Tuesday") {
 
             for (i = 0; i < 10; i++) {
-                if (window.localStorage.getItem("Local_day" + "2" + i)===" ") {
+                if (window.localStorage.getItem("Local_day" + "2" + i)==" ") {
                     index = i;
                     break;
 
@@ -223,7 +206,7 @@
         }
         else if (e.options[e.selectedIndex].value == "Wednesday") {
             for (i = 0; i < 10; i++) {
-                if (window.localStorage.getItem("Local_day" + "3" + i)===" ") {
+                if (window.localStorage.getItem("Local_day" + "3" + i)==" ") {
                     index = i;
                     break;
 
@@ -241,7 +224,7 @@
         else if (e.options[e.selectedIndex].value == "Thursday") {
 
             for (i = 0; i < 10; i++) {
-                if (window.localStorage.getItem("Local_day" + "4" + i)===" ") {
+                if (window.localStorage.getItem("Local_day" + "4" + i)==" ") {
                     index = i;
                     break;
 
@@ -259,7 +242,7 @@
         else if (e.options[e.selectedIndex].value == "Friday") {
 
             for (i = 0; i < 10; i++) {
-                if (window.localStorage.getItem("Local_day" + "5" + i)===" ") {
+                if (window.localStorage.getItem("Local_day" + "5" + i)==" ") {
                     index = i;
                     break;
 
@@ -276,7 +259,7 @@
         else if (e.options[e.selectedIndex].value == "Saturday") {
 
             for (i = 0; i < 10; i++) {
-                if (window.localStorage.getItem("Local_day" + "6" + i)===" ") {
+                if (window.localStorage.getItem("Local_day" + "6" + i)==" ") {
                     index = i;
                     break;
 
