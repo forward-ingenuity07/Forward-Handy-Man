d670cb8a7b78afe297b5c6a6156ebd399c61cccb
Forward Ingenuity <forward.ingenuity07@gmail.com>
2017-05-17 19:23:12 +0200

    Retest

--- Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
+++ Forward-Handy-Man/Forward-Handy-Man/www/scripts/schedule.js
@@ -1145,3 +1145,58 @@ document.getElementById('ButPlus').onclick = function () {
     
 }
 
+
+document.getElementById('ButClear').onclick = function () {
+    var timer, fullText, currentOffset, onComplete, wordSet;
+
+    function Speak(person, text, callback) {
+        //$("#name").html(person);
+        fullText = text;
+
+        wordSet = text.split('');
+        currentOffset = 0;
+        onComplete = callback;
+        timer = setInterval(onTick, 107);
+    }
+
+    function onTick() {
+        currentOffset++;
+        if (currentOffset == wordSet.length) {
+            complete();
+            return;
+        }
+        var text = "";
+
+        for (var i = 0; i < currentOffset; i++) {
+
+            text += wordSet[i];
+
+        }
+        text.trim();
+
+        $("#message1").html(text);
+
+    }
+
+    function complete() {
+        clearInterval(timer);
+        timer = null;
+
+        $("#message1").html(fullText);
+        if (onComplete) onComplete();
+    }
+
+    /*$(".box").click(function () {
+        complete();
+    });*/
+
+    Speak("Simon",
+        "         Should I clear the table for you?"
+    /*
+    function () {
+        setTimeout(function () {
+            Speak("Javascript", "Enter an event below");
+        }, 2000);
+    }*/);
+
+}
